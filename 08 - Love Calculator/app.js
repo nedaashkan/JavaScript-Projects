2ae028b263d63f3589e4b9cf674ff1c53a5d6250
nedaashkan <117584245+nedaashkan@users.noreply.github.com>
2023-03-26 23:19:05 +0430

    added javascript

---
+++ 08 - Love Calculator/app.js
@@ -0,0 +1,41 @@
+let headingEl = document.getElementById("heading-el");
+let yourNameEl = document.getElementById("first-name-el");
+let yourPartnerNameEl = document.getElementById("your-partner-name-el");
+let massageEl = document.getElementById("massage-el");
+let timeId;
+let startNumber;
+let lastNumber;
+function calculateLove() {
+  let yourName = yourNameEl.value;
+  let yourPartnerName = yourPartnerNameEl.value;
+  if (yourName === "" && yourPartnerName === "") {
+    massageEl.textContent = "not valid ! ";
+    massageEl.classList.add("massage-color");
+  } else if (yourName === "" || yourPartnerName === "") {
+    massageEl.textContent = "not valid ! ";
+    massageEl.classList.add("massage-color");
+  } else {
+    massageEl.classList.remove("massage-color");
+
+    timeId = setInterval(countUp, 125);
+    startNumber = 0;
+    headingEl.textContent = startNumber;
+    let randomNumber = Math.floor(Math.random() * 101);
+    lastNumber = randomNumber;
+  }
+}
+function countUp() {
+  startNumber += 1;
+  headingEl.textContent = startNumber + "%";
+  if (startNumber === lastNumber) {
+    clearTimeout(timeId);
+  }
+}
+function rest() {
+  massageEl.textContent = "";
+  massageEl.classList.remove("massage-color");
+  headingEl.textContent = "";
+  startNumber = 0;
+  yourNameEl.value = "";
+  yourPartnerNameEl.value = "";
+}
